ac8d970114410bc0ccdf0619cb73ad2511cdfa30
kartikmahajan5688 <110779952+kartikmahajan5688@users.noreply.github.com>
2023-03-19 13:44:43 +0530

    Update UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
   return (
     <div className= {color ? "header header-bg" : "header"}>
       <Link to="/">
-        <h1>Kartik Mahajan</h1>
+        <h1 id = "logo">Kartik Mahajan</h1>
       </Link>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
